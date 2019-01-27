26d1cffabded451cf5984747746a17c0bf63384d
남초이 <choee87@naver.com>
2019-01-27 13:40:26 +0900

    문제 출제를 고정값으로
    무조건 471이 문제로 나온다.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,7 +4,11 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-//		남초이의 작업 브런치입니다.
+//		컴퓨터가 낸 문제를 471로 정의.
+		int[] computerQuestionArr = {4, 7, 1};
+		
+		
+
 		
 	}
 }
