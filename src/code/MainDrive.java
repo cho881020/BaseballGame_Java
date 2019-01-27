79ea98f842b016b7834d6bb7ab25d9eb4b6561e4
Psojeong <6muse.on@gmail.com>
2019-01-27 14:51:16 +0900

    문제에 0이 있는지 없는지 검사 후, 저장

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -44,6 +44,18 @@ public class MainDrive {
 //			456 랜덤 생성 => 6를 2번칸에 저장
 			computerQuestionArr[2] = randomNumber % 10;
 			
+
+			boolean isContainZero = false; // 0이 없다고 전제.
+			
+			for (int i=0 ; i < computerQuestionArr.length ; i++) {
+				
+				if (computerQuestionArr[i] == 0) {
+//					0을 발견!
+					isContainZero = true;
+					break;
+				}
+			}
+			
 		}
 
 //		사용자에게 3자리 숫자를 입력받자.
