ccf283a9e820e4b8b94db8d95e81f540eb3dab1a
shairyu93 <shanav@naver.com>
2019-01-27 13:40:07 +0900

    문제 출제를 대략 고정값으로 함

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -3,10 +3,9 @@ package code;
 public class MainDrive {
 
 	public static void main(String[] args) {
-	
-//		조경진의 작업 브런치입니다.
-		
-//		조경진만의 추가 작업입니다.
+
+//		컴퓨터가 낸 문제를 471로 정의
+		int[] computerQuestionArr = {4, 7, 1};
 		
 	}
 }
