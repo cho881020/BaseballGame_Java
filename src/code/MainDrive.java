bdb61f447184d8a0e67a09169e2e5bf52f14816b
남초이 <choee87@naver.com>
2019-01-27 13:50:52 +0900

    사용자가 입력한 값을 배열로 분리해서 저장.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -11,7 +11,7 @@ public class MainDrive {
 		
 //		사용자에게 3자리 숫자를 입력받자.
 		Scanner scan = new Scanner(System.in);
-		System.out.println("3자리 숫자를 입력하세요 : ");
+		System.out.print("3자리 숫자를 입력하세요 : ");
 		int userInput = scan.nextInt();
 		
 //		사용자가 입력한 세자리 숫자를 저장할 배열.
@@ -20,6 +20,15 @@ public class MainDrive {
 //		1. 맨 앞자리 숫자를 0번칸에 저장한다. 423 => 4을 0번칸에 저장.
 		userInputNumArr[0] = userInput / 100;
 		
+//		2. 가운데 숫자를 1번칸에 저장. 423 => 2를 1번칸에.
+		userInputNumArr[1] = userInput / 10 % 10;
+		
+//		3. 마지막 숫자를 2번칸에 저장. 423 => 3을 2번칸에.
+		userInputNumArr[2] = userInput % 10;
+		
+		for (int i=0 ; i < userInputNumArr.length ; i++) {
+			System.out.println(userInputNumArr[i]);
+		}
 		
 
 		
