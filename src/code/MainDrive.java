0ec8f1fbc794386cb6963500515fc0680b6c11c6
soyoonyoon <starry55@naver.com>
2019-01-27 14:01:26 +0900

    중복이 있는지 검사, 조건을 다 통과하면 반복 입력을 종료함

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -42,11 +42,23 @@ public class MainDrive {
 				}
 			}
 			
-//			
+//			중복을 허가하지 않을 것. 중복인지 검사 결과 저장
+//			112,122,121 허가안함.
+			boolean hasDuplNum = (userInputNumArr[0] == userInputNumArr[1]) 
+					|| (userInputNumArr[1] == userInputNumArr[2])
+					|| (userInputNumArr[0] == userInputNumArr[2]);
 			
-			for (int i = 0; i < userInputNumArr.length; i++) {
-				System.out.println(userInputNumArr[i]);
+//			0도 없고, 동시에 중복도 없다면?
+			if (!isContainZero && !hasDuplNum) {		
+				for (int i = 0; i < userInputNumArr.length; i++) {
+					System.out.println(userInputNumArr[i]);
+				}
+				
+//				입력을 제대로 했다! => 그만 입력하도록 break;
+				break;
 			}
+			
+			
 			
 		}
 		
