64cbe78cabc0c2c934f1962cfb773cc5922c01f2
rosekathy <rosekathy@naver.com>
2019-01-27 13:40:24 +0900

    문제 출제를 고정값으로
    무조건 471이 문제로 나온다

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,7 +4,10 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-//		김미송의 작업 브런치입니다.
+//		컴퓨터가 낸 문제를 471로 정의
+		int[] computerQuestionArr = {4,7,1};
+		
+//		사용자에게 3자리 숫자를 입력받자
 		
 	}
 }
