fc64f0457297b9c8ed88954e9ef099e74d2da7d5
rosekathy <rosekathy@naver.com>
2019-01-27 14:35:36 +0900

    몇 번의 시도만에 정답을 맞췄는지

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -13,9 +13,12 @@ public class MainDrive {
 		Scanner scan = new Scanner(System.in);
 		int[] userInputNumArr = new int[3];
 
+//		몇 번 정답을 입력해봤는지 기록하는 변수
+		int inputCount = 0;
+		
 //		3S가 될때까지 계속 입력하도록
-		while(true) {
-			
+		while (true) {
+
 //			올바른 숫자를 입력해야만 검사하러 넘어감
 			while (true) {
 				System.out.print("3자리 숫자를 입력하세요 : ");
@@ -23,7 +26,7 @@ public class MainDrive {
 
 //				사용자가 123 입력 => {1,2,3} 배열로 전환
 //				사용자가 입력한 세자리 숫자를 저장할 배열
-			
+
 //				1. 맨 앞자리 숫자를 0번 칸에 저장한다. 123=> 1을 0번 칸에 저장
 				userInputNumArr[0] = userInput / 100;
 
@@ -46,13 +49,16 @@ public class MainDrive {
 
 //				중복을 허가하지 않을 것. 중복인지? 검사 결과 저장
 //				112, 122, 121 허가 안함
-				boolean hasDuplNum = (userInputNumArr[0] == userInputNumArr[1] || userInputNumArr[1] == userInputNumArr[2]
-						|| userInputNumArr[0] == userInputNumArr[2]);
+				boolean hasDuplNum = (userInputNumArr[0] == userInputNumArr[1]
+						|| userInputNumArr[1] == userInputNumArr[2] || userInputNumArr[0] == userInputNumArr[2]);
 
 //				0도 없고 동시에 중복도 업다면
 				if (!isContainZero && !hasDuplNum) {
 //					입력을 제대로 했다! => 그만 입력하도록 break;
 
+//					입력 횟수도 1 증가
+					inputCount++;
+					
 					for (int i = 0; i < userInputNumArr.length; i++) {
 						System.out.println(userInputNumArr[i]);
 					}
@@ -62,43 +68,45 @@ public class MainDrive {
 			}
 
 //			입력을 제대로 받았으니, ?S ?B인지 검사
-			
+
 //			S, B 갯수를 기록할 변수 생성.
 			int strikeCount = 0;
 			int ballCount = 0;
-			
+
 //			반복 요소
 //			1) 사용자가 입력한 숫자를 택하는 반복 : 바깥 : i
 //			2) 숫자를 가지고 문제와 비교해서 S/B 판단 : 안쪽 : j		
-			
-			for(int i = 0 ; i< userInputNumArr.length ; i++) {
-				
-				for(int j = 0 ; j<computerQuestionArr.length; j++) {
-					
-					if(userInputNumArr[i] == computerQuestionArr[j]) {
+
+			for (int i = 0; i < userInputNumArr.length; i++) {
+
+				for (int j = 0; j < computerQuestionArr.length; j++) {
+
+					if (userInputNumArr[i] == computerQuestionArr[j]) {
 //						숫자가 같다! 발견 => S인지, B인지 판단
-						if(i == j) {
+						if (i == j) {
 //							두 배열의 index가 같다? => 위치도 같다.
 //							스트라이크 갯수를 증가.
 							strikeCount++;
-						}else {
+						} else {
 //							숫자는 같은데, 위치가 다르다
 //							볼 갯수 증가
 							ballCount++;
 						}
 					}
-					
+
 				}
 			}
 			System.out.println(String.format("%d S, %d B 입니다.", strikeCount, ballCount));
-			
-			
+
 //			게임 종료 조건? 3S를 맞췄다면 ! break;
-			if(strikeCount == 3) {
+			if (strikeCount == 3) {
 				System.out.println("정답입니다!");
+				
+//				몇 번 만에 정답을 입력했는지 출력
+				System.out.println(String.format("%d번 만에 맞췄습니다.", inputCount));
 				break;
 			}
 		}
-		
+
 	}
 }
