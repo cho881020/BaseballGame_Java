49b6fe3dd8699bfdbe016963cb11dc0797e2c577
SeulkiOh <ohseulki23@naver.com>
2019-01-27 13:40:29 +0900

    문제 출제를 고정값으로.
    -무조건 471이 문제로 나온다.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -3,8 +3,8 @@ package code;
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
-		//osk_dev : 오슬기의 작업 브런치입니다.
+//		컴퓨터가 낸 문제를 471로 정의.
+		int[] computerQuestionArr = {4,7,1};
 		
 	}
 }
