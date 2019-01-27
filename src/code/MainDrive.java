6dd0fe6034cab91948389ac2e41e4b7675ca3836
monaru1124 <monaru1124@naver.com>
2019-01-27 13:44:53 +0900

    키보드 입력을 받고 맨 앞자리를 배열에 저장

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
@@ -11,6 +13,22 @@ public class MainDrive {
 		
 		
 //		사용자에게 3자리 숫자를 입력받자.
+		Scanner scan = new Scanner(System.in);
+		System.out.print("3자리 숫자를 입력하세요 : ");
+		int userInput = scan.nextInt();
+		
+//		사용자가 123 => {1,2,3} 배열로 저장.
+//		사용자가 입력한 세자리 숫자를 저장할 배열.
+		int[] userInputNumArr = new int[3];
+		
+//		1. 맨 앞자리 숫자를 0번칸에 저장한다. 123 => 1을 0번칸에 저장.
+		
+		userInputNumArr[0] = userInput / 100 ;
+		
+		
+		
+		
+		
 		
 	}
 }
